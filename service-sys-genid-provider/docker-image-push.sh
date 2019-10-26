62d7e19b067bf18ba320441a6672981368e21c80
xinxiamu <932852117@qq.com>
2019-10-26 17:14:16 +0800

    重构

--- service-sys-genid-provider/docker-image-push.sh
+++ service-sys-genid-provider/docker-image-push.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+APP_NAME_ROOT=service-sys-genid
+APP_NAME_PROVIDER=${APP_NAME_ROOT}-provider
+
+cd ..
+mvn clean install -U
+cd ${APP_NAME_PROVIDER}
+
 #构建docker 镜像并上传到私服
 mvn clean install dockerfile:build
 sleep 3
