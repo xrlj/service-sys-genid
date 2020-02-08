850b0c22682ac9cd226b54646fbaf563867bccbd
mumu <932852117@qq.com>
2020-02-08 14:13:09 +0800

    好好学习，天天向上。

--- service-sys-genid-provider/docker-image-push.sh
+++ service-sys-genid-provider/docker-image-push.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
 APP_NAME_ROOT=service-sys-genid
 APP_NAME_PROVIDER=${APP_NAME_ROOT}-provider
-
 cd ..
 mvn clean install -U
 cd ${APP_NAME_PROVIDER}
-
 #构建docker 镜像并上传到私服
-mvn clean install dockerfile:build
-sleep 3
-mvn clean install dockerfile:push
+mvn clean install dockerfile:build && mvn clean install dockerfile:push
